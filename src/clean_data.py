fde1718abfc3e564bb033b1785c1da329f4dea1b
Nooshin Mirzadeh <mirzadeh@parsapc15.epfl.ch>
2016-12-22 15:46:26 +0100

    some modification

--- src/clean_data.py
+++ src/clean_data.py
@@ -19,7 +19,8 @@ def loadTrainData(fn, emotion):
 
 
 def stemize(words, stemmer):
-  lemma_words = [WordNetLemmatizer(word) for word in words ]
+  lemma = WordNetLemmatizer()
+  lemma_words = [lemma.lemmatize(word) for word in words ]
   return [stemmer.stem(word) for word in lemma_words]
 
 def main():
@@ -36,30 +37,45 @@ def main():
   stops.update(['<user>','<url>', '.'])
   print(stops)
 
-  meaningful_X = [w for w in X if not w in stops]
+  meaningful_X = []
+  for tweet in X:
+  	meaningful_tweet = [w for w in tweet if not w in stops]
+  	meaningful_X.append(meaningful_tweet)
 
-  print(meaningful_X)
+  #print(meaningful_X)
+  meaningful_X = np.array(meaningful_X)
 
+  porter_X, lancaster_X = [],[]
   porter_stemmer = PorterStemmer()
   lancaster_stemmer = LancasterStemmer()
-  porter_X = stemize(meaningful_X, porter_stemmer)
-  lancaster_X = stemize(meaningful_X, lancaster_stemmer)
+  for i, tweet in enumerate(meaningful_X):  
+  	porter = stemize(tweet, porter_stemmer)
+  	lancaster = stemize(tweet, lancaster_stemmer)
+  	porter_X.append(porter)
+  	lancaster_X.append(lancaster)
+  	if i%1000 == 0:
+  		print(i)
   
-  print(porter_X)
-  print(lancaster_X)
+  #print(porter_X)
+  #print(lancaster_X)
   
   print('Saving the data...')
-  np.save('./results_full/labels_training', y)
-  np.save('./reulsts_full/porter_training', porter_X)
-  np.save('./reulsts_full/lancaster_training', lancaster_X)
+  np.save('./results_full/clean_labels_training', y)
+  np.save('./results_full/porter_training', np.array(porter_X))
+  np.save('./results_full/lancaster_training', np.array(lancaster_X))
 
-  with open('./clean_porter_tweets.txt') as f:
-    for w in porter_X
-      f.write((w + '\n'))
+  with open('./data/clean_porter_tweets.txt', 'w') as f:
+    for w in porter_X:
+    	for item in w:
+        	f.write((item + ' '))
+    	f.write('\n')
 
-  with open('./clean_lancaster_tweets.txt') as f:
-    for w in lancaster_X
-      f.write((w + '\n'))
+  with open('./data/clean_lancaster_tweets.txt', 'w') as f:
+    for w in lancaster_X:
+    	for item in w:
+                f.write((item + ' '))
+    	f.write('\n')
+
 
 if __name__ == '__main__':
     main()
