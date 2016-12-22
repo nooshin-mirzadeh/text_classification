6191b2ff9366559a0290ee97e79696e04df21325
mirzadeh <mirzadeh@tsf-476-wpa-1-033.epfl.ch>
2016-12-22 12:47:30 +0100

    add cross validation

---
+++ src/cross_validation.py
@@ -0,0 +1,73 @@
+import numpy as np
+import sys
+import pickle
+import sklearn as sk
+from sklearn.svm import SVC, LinearSVC, NuSVC
+from sklearn.naive_bayes import MultinomialNB, BernoulliNB, GaussianNB
+from sklearn.externals import joblib
+from sklearn.linear_model import SGDClassifier
+from sklearn.ensemble import RandomForestClassifier
+#from sklearn.neural_network import MLPClassifier
+from sklearn.externals import joblib
+import pandas as pd
+
+
+clf50 = ["SVC5000", "LinearSVC", "LinearSVC500"
+         "RandomForest",
+         "GaussianBayes", "BernoulliBayes"]
+clf200 = ["LinearSVC200", "BernoulliBayes200"]
+clf500 = ["LinearSVC500", "RandomForest500", "GaussianBayes500", "BernoulliBayes500"]
+classifiers = [
+    SVC(kernel="linear", C=0.025, max_iter=5000),
+    SVC(kernel="linear", C=0.025),
+    SVC(gamma=2, C=1),
+    LinearSVC(),
+    RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
+    GaussianNB(),
+    MultinomialNB(),
+    BernoulliNB()]
+
+def getClassifier(clf_name):
+    n2c = dict(zip(names, classifiers))
+    return n2c[clf_name]
+
+
+def main():
+    ##name_clf = input('Classifier name')
+    X50 = np.load('./results_full/bagOfWord.npy')
+    X200 = np.load('./results_full/bagOfWord200.npy')
+    X500 = np.load('./results_full/bagOfWord500.npy')
+    y = np.load('./results_full/labels_training.npy')
+    
+    scores = [(name, cross_val_score(model, X, y, cv=5).mean()) 
+                     for name, model in all_models]
+    scores = []
+    for name_clf in clf50:
+      clf = joblib.load(name_clf)
+      score = cross_val_score(clf, X50, y, cv=5).mean()
+      print(name_clf, ' : ', score)
+      scores.append((name_clf, score))
+
+    for name_clf in clf200:
+      clf = joblib.load(name_clf)
+      score = cross_val_score(clf, X200, y, cv=5).mean()
+      print(name_clf, ' : ', score)
+      scores.append((name_clf, score))
+
+    for name_clf in clf500:
+      clf = joblib.load(name_clf)
+      score = cross_val_score(clf, X50, y, cv=5).mean()
+      print(name_clf, ' : ', score)
+      scores.append((name_clf, score))	
+
+    np.save(np.array(scores))
+    print('Final results: ')
+    print(scores)
+    #with open(("./results_full/"+name_clf+"Test.csv"), 'w') as f:
+    #  np.savetxt(f, out, delimiter=',', fmt="%s")
+    print('done')
+    
+
+if __name__ == '__main__':
+    main()
+
