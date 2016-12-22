e2aea6922a4d606d1cb28ce6553a314f3038ba8b
mirzadeh <mirzadeh@tsf-476-wpa-1-033.epfl.ch>
2016-12-22 16:39:16 +0100

    classification with clean data

--- src/classify.py
+++ src/classify.py
@@ -2,27 +2,23 @@ import sys
 import numpy as np
 import pickle
 import sklearn as sk
-from sklearn.svm import SVC, LinearSVC, NuSVC
-from sklearn.naive_bayes import MultinomialNB, BernoulliNB, GaussianNB
+from sklearn.svm import LinearSVC
+from sklearn.naive_bayes import BernoulliNB, GaussianNB
 from sklearn.externals import joblib
 from sklearn.linear_model import SGDClassifier
 from sklearn.ensemble import RandomForestClassifier
 #from sklearn.neural_network import MLPClassifier
 from sklearn.externals import joblib
 
-names = ["SVC5000", "SVC", "RBFSVC", "LinearSVC",
+names = ["LinearSVC",
          "RandomForest", 
-         "GaussianBayes", "NaiveBayes", "BernoulliBayes"]
+         "GaussianBayes", "BernoulliBayes"]
 
 
 classifiers = [
-    SVC(kernel="linear", C=0.025, max_iter=5000),
-    SVC(kernel="linear", C=0.025),
-    SVC(gamma=2, C=1),
     LinearSVC(),
     RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
     GaussianNB(),
-    MultinomialNB(),
     BernoulliNB()]
 
 def getClassifier(clf_name):
@@ -31,13 +27,14 @@ def getClassifier(clf_name):
 
 
 def main():
-    name_clf = input('Classifier name')
-    X = np.load('./results_full/bagOfWord500.npy')
-    y = np.load('./results_full/labels_training.npy')
+    name_clf = input('Classifier name\n')
+    dim = input('choose the vector dimension: 50, 200, 500\n')
+    X = np.load(('./results_full/bagOfWord'+ dim + '.npy'))
+    y = np.load('./results_full/clean_labels_training.npy')
 
     clf = getClassifier(name_clf)
     clf.fit(X,y)
-    joblib.dump(clf, (name_clf+'500'))
+    joblib.dump(clf, (name_clf+dim))
 
 if __name__ == '__main__':
     main()
