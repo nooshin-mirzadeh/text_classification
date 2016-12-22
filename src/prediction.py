be4d06c634ce6be0e06d3b517d640b20267004d7
mirzadeh <mirzadeh@tsf-476-wpa-1-033.epfl.ch>
2016-12-22 17:38:17 +0100

    update prediction for clean data

--- src/prediction.py
+++ src/prediction.py
@@ -16,27 +16,17 @@ names = ["SVC5000", "SVC", "RBFSVC", "LinearSVC",
          "RandomForest",
          "GaussianBayes", "NaiveBayes", "BernoulliBayes"]
 
-classifiers = [
-    SVC(kernel="linear", C=0.025, max_iter=5000),
-    SVC(kernel="linear", C=0.025),
-    SVC(gamma=2, C=1),
-    LinearSVC(),
-    RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
-    GaussianNB(),
-    MultinomialNB(),
-    BernoulliNB()]
-
 def getClassifier(clf_name):
     n2c = dict(zip(names, classifiers))
     return n2c[clf_name]
 
-
 def main():
     name_clf = input('Classifier name')
-    X = np.load('./results_full/test500.npy')
+    dim = input('choose the dimension: 50, 200, 500')
+    X = np.load(('./results_full/test'+ dim +'.npy'))
     tid = np.load('./results_full/testID.npy')
 
-    clf = joblib.load(name_clf)
+    clf = joblib.load((name_clf+dim))
     y = clf.predict(X) 
 	
     print(y)
